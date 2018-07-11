6684a979657de3f2f7f9e2f49ffe5a0e6a62b804
Heitor Credidio <heitor.credidio@smarkets.com>
2018-07-11 17:19:09 +0100

    Add LiveQuotePlotter

--- utils.py
+++ utils.py
@@ -5,12 +5,18 @@ import threading
 import time
 from logging.config import fileConfig
 from typing import Any, Dict, List
+from itertools import count
 
 import requests
 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import pandas as pd
 from config import configuration
 
+
 log = logging.getLogger(__name__)
+
 
 class QuoteFetcher(threading.Thread):
     def __init__(self, smk_client):
@@ -76,7 +82,6 @@ class QuoteFetcher(threading.Thread):
                 transformed_quotes,
             )
 
-
     def run(self):
         self._initialize_ticker_plant()
         events = self.smk_client.get_available_events(
@@ -106,3 +111,52 @@ class Authenticator(threading.Thread):
         while True:
             time.sleep(configuration["misc"]["reauth_sleep_interval"])
             self.smk_client.reauth_session()
+
+
+class LiveQuotePlotter(threading.Thread):
+    def __init__(self, smk_client):
+        self.smk_client = smk_client
+
+    def run(self, market_id):
+        market = self.smk_client.get_markets([market_id])[0]
+        contracts = self.smk_client.get_related_contracts([market])
+        contract_ids = ','.join([contract['id'] for contract in contracts])
+
+        fig, ax = plt.subplots()
+        connection = sqlite3.connect('tickerplant.db')
+
+        def frames(_):
+            with connection:
+                df = pd.read_sql(
+                    'SELECT * FROM ticks WHERE contract_id IN ({})'.format(contract_ids),
+                    connection, parse_dates=['timestamp'],
+                )
+            xlim = ax.get_xlim()
+            ylim = ax.get_ylim()
+            ax.clear()
+            df = (df.set_index(['timestamp', 'contract_id'])
+                [['bp1', 'op1']]
+                .unstack('contract_id')
+            )
+            df.groupby(level=1, axis=1).plot(
+                ax=ax, marker='.', drawstyle='steps-post', color=['C0', 'C1', 'C2']
+            )
+            ax.legend([contract['slug'] for contract in contracts])
+            if xlim != (0.0, 1.0) and ylim != (0.0, 1.0):
+                ax.set_xlim(xlim)
+                ax.set_ylim(ylim)
+            return ax.get_lines()
+
+        ani = FuncAnimation(
+            fig, frames, frames=count(), blit=False,
+            interval=configuration['misc']['sleep_interval'] * 1000,
+        )
+        plt.show()
+
+
+if __name__ == '__main__':
+    import client
+    import logging
+    logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.INFO)
+    cli = client.SmarketsClient()
+    QuoteFetcher(cli).run()
